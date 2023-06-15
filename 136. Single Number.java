58463ba6c6df169187f9830da45bb14878bc3908
Samin Ahsan <turzo68@gmail.com>
2023-06-15 18:19:36 +0600

    solved task 136

---
+++ 136. Single Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> numCounts = new HashMap<Integer, Integer>();
+
+        for (int num : nums) {
+            if (!numCounts.containsKey(num)) {
+                numCounts.put(num, 1);
+            } else {
+                numCounts.put(num, numCounts.get(num) + 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> entry: numCounts.entrySet()) {
+            if(entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return 1;
+    }
+}
