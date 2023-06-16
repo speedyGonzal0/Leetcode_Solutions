7087f209eee6b4839c6841703b01592bc133ce9d
Samin Ahsan <turzo68@gmail.com>
2023-06-17 01:31:46 +0600

    solved task 27

---
+++ 27. Remove Element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        if (nums.length == 1 && nums[0] != val) {
+            k++;
+            return k;
+        }
+
+        for (int i = 0, j = nums.length - 1; i <= j; ) {
+            if (nums[i] == val && nums[j] != val) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                k++;
+                i++;
+                j--;
+            } else if (nums[i] != val && nums[j] == val) {
+                k++;
+                i++;
+            } else if (nums[i] == val && nums[j] == val) {
+                j--;
+            } else if (nums[i] != val && nums[j] != val) {
+                k++;
+                i++;
+            }
+
+        }
+        return k;
+    }
+}
