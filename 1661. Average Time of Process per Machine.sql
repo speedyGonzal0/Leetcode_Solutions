7f4720c9d74de195dcf0d636cfb5d8eee54b82d0
Samin Ahsan <turzo68@gmail.com>
2023-07-15 11:24:59 +0600

    solved task 1661

---
+++ 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,1 @@
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) as processing_time FROM (SELECT * FROM Activity WHERE activity_type = 'start' ORDER BY process_id) a1 INNER JOIN (SELECT * FROM Activity WHERE activity_type = 'end' ORDER BY process_id) a2 ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id GROUP BY a1.machine_id;
