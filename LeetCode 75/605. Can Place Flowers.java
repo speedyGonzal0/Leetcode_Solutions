a314f5f8db1064d473b085fa91723804c0401197
Samin Ahsan <turzo68@gmail.com>
2023-09-14 02:40:45 +0600

    solved task 605

---
+++ LeetCode 75/605. Can Place Flowers.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+//        for (int i = 0; i < flowerbed.length && n > 0; i++){
+//
+//            if (flowerbed[i] == 0){
+//                if (i == 0){
+//                    if (i + 1 < flowerbed.length && flowerbed[i + 1] == 0){
+//                        flowerbed[i] = 1;
+//                        n--;
+//                    }
+//                    else if (i + 1 >= flowerbed.length){
+//                        flowerbed[i] = 1;
+//                        n--;
+//                    }
+//                }
+//
+//                else if (i == flowerbed.length - 1 && flowerbed[i - 1] == 0){
+//                    flowerbed[i] = 1;
+//                    n--;
+//                }
+//
+//                else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0){
+//                    flowerbed[i] = 1;
+//                    n--;
+//                }
+//            }
+//
+//        }
+//
+//        return n == 0;
+
+        for (int i = 0; i < flowerbed.length; i++){
+
+            if (n == 0){
+                return true;
+            }
+
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)){
+                flowerbed[i] = 1;
+                n--;
+            }
+
+        }
+
+        return n == 0;
+    }
+}
